3a84f7e2964ef178ac3d75cb5b7efb430944b1ee
Jeff Griffiths <jeff@canuckistani.ca>
2011-10-17 23:37:16 -0700

    well at least we have one test.

--- test/test-main.js
+++ test/test-main.js
@@ -1,5 +1,12 @@
-const main = require("main");
+var shortener = require("main");
+var testRunner, remaininTests;
 
-exports.test_test_run = function(test) {
-  test.pass("Unit test running!");
-};
+exports.test_shorten = function(test) {
+    testRunner = test;
+    testRunner.waitUntilDone(2000);
+    shortener.shorten('https://www.mozilla.org/', function(response) {
+        console.log('url is: ' + response.id);
+        testRunner.assertEqual(/^http:\/\/goo.gl\/[\w]{5}/.test(response.id), true);
+        testRunner.done();
+    });
+}
